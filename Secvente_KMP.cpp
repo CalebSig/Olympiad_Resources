a08f180b43dd576e7d450bb0a7d295b3df69dc1d
CalebSig <Sighiartau.Do.Achim@student.utcluj.ro>
2025-10-21 17:58:24 +0300

    Update Secvente_KMP.cpp

--- Secvente_KMP.cpp
+++ Secvente_KMP.cpp
@@ -9,7 +9,7 @@ void construire(string w, int T[]) ///precalcularea tabelei T, T[i]=j => de la p
     int k = -1;
     int i = 1;
 
-    while (i < m) 
+    while (i < m)
     {
         while (k >= 0 && w[k + 1] != w[i])
             k = T[k];
@@ -34,7 +34,7 @@ void KMP(string s, string w)
     int n = s.size(), m = w.size();
     int i = 0, k = -1; ///indicele lui s, indicele lui w
 
-    while (i < n) 
+    while (i < n)
     {
         while (k >= 0 && w[k + 1] != s[i])
             k = T[k];
@@ -43,7 +43,7 @@ void KMP(string s, string w)
         if (k == m - 1) ///match
         {
             cout << "Sir gasit la: " << i - m + 1 << endl;
-            k = T[k]; 
+            k = T[k];
         }
         ++i;
     }
